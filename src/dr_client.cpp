c646ceeb97c8a7fb0c44da930d2ff94477e5dc85
illera88 <agarciaillera@gmail.com>
2018-04-24 16:20:28 +0200

    reduced example

--- src/dr_client.cpp
+++ src/dr_client.cpp
@@ -1,19 +1,26 @@
+#include <dr_api.h>
 
-#include "dr_api.h"
+void create_IDA_symbols_file(void* image_base) {
+	dr_printf("before system\n");
+	STARTUPINFO info = { sizeof(info) };
+	PROCESS_INFORMATION processInfo;
+	if (CreateProcess(NULL, "echo aaaa > C:\\Users\\default.LAPTOP-BGOSJ7N3\\Desktop\\perro.txt", NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
+	{
+		WaitForSingleObject(processInfo.hProcess, INFINITE);
+		CloseHandle(processInfo.hProcess);
+		CloseHandle(processInfo.hThread);
+	}
+	//system("echo aaaa > C:\\Users\\default.LAPTOP-BGOSJ7N3\\Desktop\\perro.txt");
+	dr_printf("after system\n");
 
-
-DR_EXPORT void
-dr_client_main(client_id_t id, int argc, const char *argv[])
-{
-    dr_set_client_name("DynamoRIO Sample Client",
-                       "http://dynamorio.org/issues");
-
-#ifdef _WIN32
-	dr_enable_console_printing();
-#endif	
-
-	dr_printf("This must be printed\n");
-	dr_abort();
 }
 
 
+DR_EXPORT void dr_client_main(client_id_t id, int argc, const char *argv[])
+{
+    dr_enable_console_printing();
+
+	dr_printf("before\n");
+	create_IDA_symbols_file(NULL);
+	dr_printf("after\n");
+}
